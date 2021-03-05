321a45a933563f9990f4b360a4cf5905ec80bef2
crypticCaroline <rebecca.kelsall@sky.com>
2021-03-05 14:21:29 +0000

    Added basic html to test database in upload_image.html and upload_video.html

--- app.py
+++ app.py
@@ -17,20 +17,53 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-cloudinary.config( 
-  cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME"),
-  api_key = os.environ.get("CLOUDINARY_API_KEY"), 
-  api_secret = os.environ.get("CLOUDINARY_API_SECRET") 
+cloudinary.config(
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET")
 )
 
 mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/homepage")
+@app.route("/homepage", methods=["GET", "POST"])
 def homepage():
+    if request.method == "POST":
+        mongo.db.users.insert_one({"username": request.form.get("username")})
     return render_template("index-test.html")
 
+
+@app.route("/profile/<username>/upload/", methods=["GET", "POST"])
+def upload_image(username):
+
+    if request.method == 'POST':
+        for item in request.files.getlist("image_file"):
+            filename = secure_filename(item.filename)
+            filename, file_extension = os.path.splitext(filename)
+            public_id_image = (username + '/' + filename)
+            public_id_video = ("vidoes/" + username + "/" + filename)
+            cloudinary.uploader.unsigned_upload(
+                item, "puppy_image", cloud_name='puppyplaymates',
+                folder='/doubleshamrocks/', public_id=public_id_image)
+            cloudinary.uploader.unsigned_upload(
+                user_video, "puppy_image",
+                cloud_name='puppyplaymates',
+                folder='/doubleshamrocks/', public_id=public_id_image,
+                resource_type="video")
+            image_url = (
+                "https://res.cloudinary.com/puppyplaymates/image/upload/doubleshamrocks/"
+                + public_id_image + file_extension)
+            video_url = (
+                "https://res.cloudinary.com/puppyplaymates/image/upload/doubleshamrocks/"
+                + public_id_video + file_extension)
+
+            mongo.db.users.update_one(
+                {"username": session["user"]},
+                {"$addToSet": {"user_images": image_url}})
+
+            return redirect(url_for('profile_image', username=username))
+            return render_template("profile_image.html", username=username)
 
 
 if __name__ == "__main__":
