923f6d89bf60c2860ce3ab54914acd24a77e0d71
Irina <iratushina17@gmail.com>
2021-03-07 23:34:48 +0000

    My videos section added to the profile page

--- app.py
+++ app.py
@@ -408,10 +408,12 @@ def profile(username):
                                      session['username']})['profile_image']
     user_id = mongo.db.users.find_one({'username': session['username']})['_id']
     my_jokes = mongo.db.jokes.find({'user': user_id})
+    my_videos = mongo.db.videos.find({'user': user_id})
     return render_template('profile.html',
                            image=image,
                            username=username,
-                           my_jokes=my_jokes)
+                           my_jokes=my_jokes,
+                           my_videos=my_videos)
 
 
 # Change username
