8017d98c2471c9ddd2702834167ea64840629446
alexeykuz-sys <alexeykuz@gmail.com>
2021-03-07 13:47:19 +0000

    ad

---
+++ static/script.js
@@ -0,0 +1,9 @@
+// let likeBtn = document.getElementById('like-score');
+
+function likeBtnHandler(){
+    count = 0
+    console.log(document.querySelector('.score-likes').innerHTML)
+    console.log('gfgfd')
+}
+
+document.getElementById('like-score').addEventListener('click', likeBtnHandler)
