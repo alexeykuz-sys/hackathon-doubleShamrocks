4eda53ab83797feeb42057974dadb58c9ea884e0
crypticCaroline <rebecca.kelsall@sky.com>
2021-03-08 09:51:46 +0000

    Added start of progress bar

--- static/script.js
+++ static/script.js
@@ -4,3 +4,28 @@ $(document).ready(function(){
    */
   $('.toast').toast('show');
 });
+
+
+$(function() {
+
+    var bar = $('.bar');
+    var percent = $('.percent');
+    var status = $('#status');
+
+    $('.upload_form')({
+        beforeSend: function() {
+            status.empty();
+            var percentVal = '0%';
+            bar.width(percentVal);
+            percent.html(percentVal);
+        },
+        uploadProgress: function(event, position, total, percentComplete) {
+            var percentVal = percentComplete + '%';
+            bar.width(percentVal);
+            percent.html(percentVal);
+        },
+        complete: function(xhr) {
+            status.html(xhr.responseText);
+        }
+    });
+});
