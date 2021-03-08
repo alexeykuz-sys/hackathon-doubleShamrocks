16f8be8626b6b70ca928a9141c9eac2337cbdd94
alexeykuz-sys <alexeykuz@gmail.com>
2021-03-08 14:58:14 +0000

    change debugger

--- app.py
+++ app.py
@@ -629,4 +629,4 @@ def page_not_found(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
